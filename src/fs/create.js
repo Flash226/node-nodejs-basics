c96c7d6f51ff3717810f364cdbdda0bad97fcd86
Yaroslav Nepiuk <slavanepuk@gmail.com>
2023-06-19 20:35:21 +0200

    feat: work with the file system using FS has been implemented

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const folderPath = path.resolve(__dirname, 'files');
+  const filePath = path.join(folderPath, 'fresh.txt');
+  const fileContent = 'I am fresh and young';
+
+  try {
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath);
+    }
+
+    if (fs.existsSync(filePath)) {
+      throw new Error('Error: File already exists');
+    }
+
+    await fs.promises.writeFile(filePath, fileContent);
+  } catch (err) {
+    console.error('FS operation failed:', err.message);
+  }
 };
 
 await create();
--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,28 @@
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const remove = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const folderPath = path.join(__dirname, 'files');
+  const fileName = 'fileToRemove.txt';
+
+  try {
+    const filePath = path.join(folderPath, fileName);
+
+    try {
+      await fs.access(filePath, fs.constants.F_OK);
+    } catch (err) {
+      throw new Error('Error: File does not exist');
+    }
+
+    await fs.unlink(filePath);
+
+  } catch (err) {
+    console.error('FS operation failed:', err.message);
+  }
 };
 
 await remove();
