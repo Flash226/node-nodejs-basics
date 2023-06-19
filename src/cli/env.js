007475fc7550063a4d363702efe229c1b805fa1a
Yaroslav Nepiuk <slavanepuk@gmail.com>
2023-06-19 20:52:39 +0200

    feat: work with cli has been implemented

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
-};
-
-parseEnv();
+    const prefix = 'RSS_';
+    const envVariables = process.env;
+  
+    for (const key in envVariables) {
+      if (key.startsWith(prefix)) {
+        const value = envVariables[key];
+        const formattedKey = key.replace(prefix, '');
+        console.log(`${key}=${value}`);
+      }
+    }
+  };
+  
+  parseEnv();
